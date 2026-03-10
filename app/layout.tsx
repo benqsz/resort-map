07a107fb4d98880b32d11691583c21a1dc2602e7
benqsz <beniamink123@gmail.com>
2026-03-10 16:15:58 +0100

    feat: project init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,18 @@
-import { Geist, Geist_Mono, Nunito_Sans } from "next/font/google"
+import { Nunito_Sans } from "next/font/google"
 
 import "./globals.css"
+
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-const nunitoSans = Nunito_Sans({variable:'--font-sans'})
+const nunitoSans = Nunito_Sans({ variable: "--font-sans" })
 
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", nunitoSans.variable)}
+      className={cn("antialiased", "font-sans", nunitoSans.variable)}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
